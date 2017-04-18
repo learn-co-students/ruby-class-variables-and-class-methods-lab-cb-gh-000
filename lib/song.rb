3657b805fc935f26e8255ad7610e3b062f836987
Dan Strong <dangaston@gmail.com>
2017-04-18 05:29:01 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,13 +2,13 @@ class Song
 
   @@count = 0
   @@genres = []
-  @@artists
+  @@artists = []
 
-  def initialize :name, :artist, :genre
+  def initialize name, artist, genre
     @@name = name
     @@artist = artist
-    @@artists << artist
     @@genre = genre
+    @@artists << artist
     @@genres << genre
     @@count += 1
   end
@@ -40,29 +40,27 @@ class Song
   end
 
   def self.genre_count
-    genre_hash = {}
+    histogram = {}
     @@genres.each do |genre|
-      if genre_hash.has_value?(genre)
-        genre_hash[genre] += 1
+      if histogram[genre] == nil
+        histogram[genre] = 1
       else
-        genre_hash << genre
-        genre_hash[genre] += 1
+        histogram[genre] += 1
       end
     end
-  genre_hash
+  histogram
   end
 
   def self.artist_count
-    artist_hash = {}
-    @@artists.each do |genre|
-      if artist_hash.has_value?(artist)
-        artist_hash[artist] += 1
-      else
-        artist_hash << artist
-        artist_hash[genre] += 1
+	    histogram = {}
+      @@artists.each do |artist|
+        if histogram[artist] == nil
+          histogram[artist] = 1
+        else
+          histogram[artist] += 1
+        end
       end
-    end
-  artist_hash
+    histogram
   end
 
 end
