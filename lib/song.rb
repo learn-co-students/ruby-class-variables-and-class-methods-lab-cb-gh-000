398d95242468042c87548132043593cbf9c23f77
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-11 01:37:03 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -12,25 +12,59 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
-
-    if !@@artists.include?(artist)
-      @@artists << artist
-    end
-    if !@@genres.include?(genre)
-      @@genres << genre
-    end
+    @@artists << artist
+    @@genres << genre
   end
 
-  def count
+  def self.count
     @@count
   end
 
-  def artists
-    @@artists
+  def self.artists
+    ret = []
+    @@artists.each do |artist|
+      if !ret.include?(artist)
+        ret << artist
+      end
+    end
+    ret
   end
 
-  def genres
-    @@genres
+  def self.genres
+    ret = []
+    @@genres.each do |genre|
+      if !ret.include?(genre)
+        ret << genre
+      end
+    end
+    ret
   end
 
+  def self.genre_count
+    ret = {}
+    self.genres.each do |genre1|
+      sum = 0
+      @@genres.each do |genre2|
+        if genre1 == genre2
+          sum += 1
+        end
+      end
+      ret[genre1] = sum
+    end
+    ret
+  end
+
+  def self.artist_count
+    ret = {}
+    self.artists.each do |artist1|
+      sum = 0
+      @@artists.each do |artist2|
+        if artist1 == artist2
+          sum += 1
+        end
+      end
+      ret[artist1] = sum
+    end
+    ret
+  end
 end
